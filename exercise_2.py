8ac1374e4e335948c4621ddf961804c23ab2d917
Red A <hey.ccaa@gmail.com>
2015-04-08 13:53:54 -0600

    updated exercsise 2 for multiple lines

--- exercise_2.py
+++ exercise_2.py
@@ -8,5 +8,7 @@ co2produced = gasgals * 20
 ethanolreqd = (gasgals * 115000)/75700
 price = gasgals * 4.0
 
-print "You entered %f \n which is %.2f literes and \n required %.2f barrels to produce \n which produces %.2f lbs of CO2 \n and costs %.2f dollars \n Equivalent Ethanaol required would be %.2f" %(gasgals, lit, barrels, co2produced, price, ethanolreqd)
+print "You entered %f \n which is %.2f literes and \n required %.2f barrels to produce \n " \
+      "which produces %.2f lbs of CO2 \n and costs %.2f dollars \n Equivalent Ethanaol required would be %.2f"\
+      %(gasgals, lit, barrels, co2produced, price, ethanolreqd)
 
